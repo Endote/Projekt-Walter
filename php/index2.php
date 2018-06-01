06e34043f52a739295361447a92ee7a75b9acf69
Arrashi <n.weronika@op.pl>
2018-06-01 21:57:03 +0200

    login/registration added.

---
+++ php/index2.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<meta charset="utf-8" />
+<title>Registration</title>
+</head>
+
+<body>
+<h3>Register</h3>
+<form name="form" action="registration.php" method="post">
+Login: <input type="text" name="login" autocomplete="off"> <br />
+Password: <input type ="password" name="password"> <br />
+Confirm password: <input type ="password" name="password2"> <br />
+Email: <input type ="text" name="email"autocomplete="off">  <br />
+<input type="submit" value="Register">
+</form>
+
+</body>
+
+
+</html
---
+++ php/login.php
@@ -0,0 +1,43 @@
+<?php 
+
+require_once 'connect.php';
+
+if($link === false)
+{
+    die("ERROR: Could not connect. ".mysqli_connect_error());
+}
+
+$login = $_POST['login'];
+$password = SHA1($_POST['password']);
+$result = mysqli_query($link,"SELECT Login FROM login WHERE Login='$login' OR Email='$login'");
+$loginDB = mysqli_num_rows($result) > 0 ? 'yes' : 'no';
+$result2 = mysqli_query($link,"SELECT Password FROM login WHERE Login='$login' OR Email='$login'");
+$row = mysqli_fetch_row($result2);
+$passwordDB = substr($row[0], 32);
+
+
+if (($loginDB == 'no') || ($passwordDB !== $password))
+{
+    echo "<script type='text/javascript'>alert('Invaild login/email or password');window.location = 'index.php';</script>";
+}
+
+else
+{
+    $verification = mysqli_query($link,"SELECT EmailStatus FROM login WHERE Login='$login' OR Email='$login'");
+    $row2 = mysqli_fetch_row($verification);
+    if ($row2[0] == 'not verified')
+    {
+        echo "<script type='text/javascript'>alert('You must activate your account first.');window.location = 'index.php';</script>";
+    }
+    
+    else if (($loginDB == 'yes') && ($passwordDB == $password) && ($row2[0] == 'verified') )
+    {
+        echo "You are logged in! :)";
+        echo "<br /><br /> <a href='index.php'>Back</a>";
+    }
+    
+    mysqli_close($link);
+    
+}
+
+?>
