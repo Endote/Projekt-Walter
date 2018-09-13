afefe5721e31e291c1f619b073b34f56416fa945
Radek <rstepien@onet.eu>
2018-09-13 22:53:01 +0200

    Database update

--- php/login.sql
+++ php/login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 26, 2018 at 05:46 PM
+-- Generation Time: Sep 13, 2018 at 10:52 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -33,6 +33,7 @@ CREATE TABLE `login` (
   `Login` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `Password` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `Email` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Contact_Name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `Phone` int(11) NOT NULL,
   `ActivationCode` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `EmailStatus` enum('not verified','verified') NOT NULL,
@@ -43,12 +44,14 @@ CREATE TABLE `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`ID`, `Login`, `Password`, `Email`, `Phone`, `ActivationCode`, `EmailStatus`, `Favourites`) VALUES
-(22, 'test4', '1b8xtho0cked1bcazh2xwn0fwgag2zflcb45c671cbc500627ea424eea5f91996221b5935', 'dfdgfd@op.pl', 0, 'e820a45f1dfc7b95282d10b6087e11c0', 'verified', '1.2'),
-(23, 'test', 'ygjw9zn7winr8oqb18kp1wp4i7uqr9qscb45c671cbc500627ea424eea5f91996221b5935', 'sdfwseddewger@op.pl', 0, '9de6d14fff9806d4bcd1ef555be766cd', 'not verified', '1'),
-(25, 'Rade2', 'cdb03ee189d5676b3d2dea78d520035167d764e6f3e1be2fa9479a7e0df9000a401daf80', 'b1925720@nwytg.net', 123456789, '069d3bb002acd8d7dd095917f9efe4cb', 'verified', '2'),
-(26, 'Rade7', '089a232dcc8dd8db3de554134681520e3ee97343c7065d6407a39381f8f02d07340beb67', 'asdfwef@op.pl', 234567891, 'd707329bece455a462b58ce00d1194c9', 'not verified', ''),
-(28, 'Rade9', '9722556a0094c9b5d8b3d517271929ee3ee97343c7065d6407a39381f8f02d07340beb67', 'asdasd@op.pl', 234567891, 'e2c420d928d4bf8ce0ff2ec19b371514', 'verified', '1.2');
+INSERT INTO `login` (`ID`, `Login`, `Password`, `Email`, `Contact_Name`, `Phone`, `ActivationCode`, `EmailStatus`, `Favourites`) VALUES
+(22, 'test4', '1b8xtho0cked1bcazh2xwn0fwgag2zflcb45c671cbc500627ea424eea5f91996221b5935', 'dfdgfd@op.pl', 'Camellia Kamakshi', 0, 'e820a45f1dfc7b95282d10b6087e11c0', 'verified', '1.2'),
+(23, 'test', 'ygjw9zn7winr8oqb18kp1wp4i7uqr9qscb45c671cbc500627ea424eea5f91996221b5935', 'sdfwseddewger@op.pl', 'Pero Jadranko', 0, '9de6d14fff9806d4bcd1ef555be766cd', 'not verified', '1'),
+(25, 'Rade2', 'cdb03ee189d5676b3d2dea78d520035167d764e6f3e1be2fa9479a7e0df9000a401daf80', 'b1925720@nwytg.net', ' Zvezdana Menno', 123456789, '069d3bb002acd8d7dd095917f9efe4cb', 'verified', '2'),
+(26, 'Rade7', '089a232dcc8dd8db3de554134681520e3ee97343c7065d6407a39381f8f02d07340beb67', 'asdfwef@op.pl', 'Rahela Tatiana', 234567891, 'd707329bece455a462b58ce00d1194c9', 'not verified', ''),
+(28, 'Rade9', '9722556a0094c9b5d8b3d517271929ee3ee97343c7065d6407a39381f8f02d07340beb67', 'asdasd@op.pl', 'Dipika Mark', 234567891, 'e2c420d928d4bf8ce0ff2ec19b371514', 'verified', '1.2'),
+(29, 'Toto', '0d99d35ee7794e323d87d8b08a746710db59277f30a99d3597362b54ed7c090b7d1b23bd', 'africa@gmail.com', 'Clarissa Dianna', 123456789, '7250eb93b3c18cc9daa29cf58af7a004', 'not verified', ''),
+(30, 'Toto2', 'ae3b631601bdce5aa8d259b1da5532e56180090cbaa9eb066d528ead2a2b6ff2d6b621d4', 'afric2a@gmail.com', 'David Paich', 123456789, '1ecfb463472ec9115b10c292ef8bc986', 'verified', '');
 
 --
 -- Indexes for dumped tables
@@ -68,7 +71,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
