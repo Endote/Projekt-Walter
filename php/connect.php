235ab1002d3393ad59fd08203cc71e3a7b254c0e
Weronika <32653324+Arrashi@users.noreply.github.com>
2018-10-11 12:08:58 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,11 @@
 <?php
-
-$link = mysqli_connect("localhost","root","","test");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$link = mysqli_connect("exten.nazwa.pl","exten_inkapital","Walter514","exten_inkapital");
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 ?>
