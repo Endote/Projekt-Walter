4a2db75dea152d8ff8cc79da2a18dc8c9325add2
Weronika <32653324+Arrashi@users.noreply.github.com>
2018-10-11 13:11:13 +0200

    Update settings.php

--- AccountIframe/settings.php
+++ AccountIframe/settings.php
@@ -104,7 +104,7 @@ function Confirm(){
   $session_user_id = $_SESSION['user']; 
   function random_str($length, $keyspace = '0123456789abcde')
   {
-    $pieces = [];
+    $pieces = array();
     $max = mb_strlen($keyspace, '8bit') - 1;
     for ($i = 0; $i < $length; ++$i) {
         $pieces []= $keyspace[random_int(0, $max)];
